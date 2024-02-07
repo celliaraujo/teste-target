2d1342543b1ae00d8998f6d2060cf4a22c964fe0
celliaraujo <celliaraujo@gmail.com>
2024-02-07 16:22:17 -0300

    percentual-estado

---
+++ percentual-estado/percentual.js
@@ -0,0 +1,15 @@
+let estados = ["SP", "RJ", "MG", "ES", "OUTROS"];
+let valores = [67836.43, 3678.66, 29229.88, 27165.48, 19849.53];
+
+let valorTotal = 0;
+
+valores.forEach((valor) => {
+    valorTotal+=valor;
+});
+
+console.log
+console.log('Percentual de representação mensal por estado:');
+valores.forEach((valor, index) =>{
+    let percentual = ((valor / valorTotal) * 100).toFixed(2);
+    console.log(`${estados[index]} foi de ${percentual}%.`);
+});
